987f263c6e64d10b41235aaa561a7c67c451ac61
Linapush <linapush2002@gmail.com>
2024-09-01 15:46:57 +0300

    download habdler, other version

--- webapp/api/file/file.py
+++ webapp/api/file/file.py
@@ -8,6 +8,7 @@ from fastapi.encoders import jsonable_encoder
 from minio.error import S3Error
 from sqlalchemy.ext.asyncio import AsyncSession
 from webapp.logger import logger
+from io import BytesIO
 
 from webapp.api.file.router import file_router
 from webapp.crud.file import create_file
@@ -100,27 +101,62 @@ async def get_filtered_files_endpoint(
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
 
 
-@file_router.get('/download/{file_id}', response_class=StreamingResponse, tags=['file'])
+#########################################################################################################################################
+# @file_router.get('/download/{file_id}', response_class=StreamingResponse, tags=['file'])
+# async def download_file_endpoint(
+#     file_id: int,
+#     session: AsyncSession = Depends(get_session),
+#     access_token: JwtTokenT = Depends(jwt_auth.get_current_user),
+# ):
+#     try: # получаем запись файла из базы данных
+#         file_record = await download_file_by_user_id_and_file_id(
+#             session=session, user_id=access_token['user_id'], file_id=file_id
+#         )
+#         if not file_record:
+#             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Файл не найден')
+#         # получаем объект из MinIO
+#         response = minio_client.get_object(f'user-{access_token["user_id"]}', file_record.file_path)
+#         safe_filename = quote(file_record.file_name)            # кодируем имя файла для безопасного использования в заголовках
+#         headers = {'Content-Disposition': f'attachment; filename"{safe_filename}'}
+#         return StreamingResponse(                               # тип ответа, который  позволяет  потоково  передавать  большие  файлы
+#             response.stream(32 * 1024),
+#             media_type=file_record.file_type,
+#             headers=headers,
+#         )
+#     except S3Error as e:
+#         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f'Ошибка хранилища: {str(e)}')
+#     except Exception as e:
+#         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
+#########################################################################################################################################
+
+
+@file_router.get('/download/{file_id}', tags=['file'])
 async def download_file_endpoint(
     file_id: int,
     session: AsyncSession = Depends(get_session),
     access_token: JwtTokenT = Depends(jwt_auth.get_current_user),
 ):
+    bucket_name = f'user-{access_token["user_id"]}'
+    
     try:
         file_record = await download_file_by_user_id_and_file_id(
             session=session, user_id=access_token['user_id'], file_id=file_id
         )
+        
         if not file_record:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Файл не найден')
-
-        response = minio_client.get_object(f'user-{access_token["user_id"]}', file_record.file_path)
-        safe_filename = quote(file_record.file_name)            # кодируем имя файла для безопасного использования в заголовках
-        headers = {'Content-Disposition': f'attachment; filename*=utf-8""{safe_filename}'}
-        return StreamingResponse(                               # тип ответа, который  позволяет  потоково  передавать  большие  файлы
-            response.stream(32 * 1024),
-            media_type=file_record.file_type,
-            headers=headers,
-        )
+        
+        response = minio_client.get_object(bucket_name, file_record.file_path)
+        
+        data = BytesIO(response.read())
+        response.close()
+        response.release_conn()
+        
+        safe_filename = quote(file_record.file_name)
+        headers = {'Content-Disposition': f'attachment; filename="{safe_filename}"'}
+        
+        return StreamingResponse(data, media_type=file_record.file_type, headers=headers)
+    
     except S3Error as e:
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f'Ошибка хранилища: {str(e)}')
     except Exception as e:
